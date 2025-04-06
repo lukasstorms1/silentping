c6c021fc7758cddad087e15f69fbfad8b95c5705
lukasstorms1 <85074024+lukasstorms1@users.noreply.github.com>
2025-04-06 18:22:25 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1 style={{ fontSize: "2rem" }}>SilentPing 🚀</h1>
+      <p>This is your official project homepage. Deployed & owned by Lukas 😎</p>
+    </div>
+  );
+}
